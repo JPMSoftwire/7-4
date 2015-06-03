1c66256a920c08e590161ff6f930590601d5c938
Jake Mckenna <Jake.Mckenna@softwire.com>
2015-06-03 09:30:37 +0100

    Review comments

--- 7-4/Program.cs
+++ 7-4/Program.cs
@@ -11,6 +11,7 @@ namespace _7_4
         static void Main(string[] args)
         {
             Square s = new Square(new Point(0, 0), 5);
+            // Using the name 'square' would be better.
             s.display();
         }
     }
@@ -19,11 +20,13 @@ namespace _7_4
         public int x;
         public int y;
         public Point(int xCoOrd, int yCoOrd)
+        // Again, you can just call your parameters x and y, then use this.x = x etc
         {
             x = xCoOrd;
             y = yCoOrd;
         }
         public void display()
+        // Again, capitalise your methods. 
         {
             Console.WriteLine("({0},{1})", x, y);
         }
@@ -33,6 +36,11 @@ namespace _7_4
         private Point point;
         private int length;
         public Square(Point p, int l)
+        // Again, naming your parameters to your constructor is important here. Imagine someone was using this class
+        // elsewhere in their code - in order to understand what the parameters p and l mean, they'd have to 
+        // jump into this file and see that they refer to the point and the length, and then remember that when they jump
+        // back to their code. You could also name them better than 'point' and 'length' - maybe 'bottomLeftCorner'
+        // and 'sideLength', for instance.
         {
             point = p;
             length = l;
